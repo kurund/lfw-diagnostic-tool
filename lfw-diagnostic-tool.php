c023886298dfa84f67f4509541b373392ae26923
Kurund Jalmi <kurund@yahoo.com>
2017-08-17 17:44:03 +0530

    basic project setup

---
+++ lfw-diagnostic-tool.php
@@ -0,0 +1,11 @@
+<?php
+/*
+  Plugin Name: Diagnostic Tools for LFW
+  Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+  Description: This plugin gives recommendations based on the data provided by the end user
+  Version: 1.0
+  Author: Kurund Jalmi
+  Author URI: http://URI_Of_The_Plugin_Author
+ License: GPL2
+ */
+
